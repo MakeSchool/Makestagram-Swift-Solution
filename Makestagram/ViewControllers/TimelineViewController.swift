8ae4e296701feb4731fa120305eca60d11efe5c8
Ben-G <Benjamin.Encz@gmail.com>
2015-05-21 13:16:22 -0700

    [STEP] Photo button working

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -6,30 +6,29 @@
 //  Copyright (c) 2015 Make School. All rights reserved.
 //
 
-import UIKit
+    import UIKit
 
-class TimelineViewController: UIViewController {
+    class TimelineViewController: UIViewController {
 
-    override func viewDidLoad() {
+      override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+      }
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Tab Bar Delegate
+
+    extension TimelineViewController: UITabBarControllerDelegate {
+      
+      func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+          println("Take Photo")
+          return false
+        } else {
+          return true
+        }
+      }
+      
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
