819ffa0d695aac580be0eb5549e49df32abbdadf
Ben-G <Benjamin.Encz@gmail.com>
2015-05-22 11:34:06 -0700

    [STEP] Set up basic PhotoTakingHelper
    Connected PhotoTakingHelper to TimelineViewController

---
+++ Makestagram/PhotoTaking/PhotoTakingHelper.swift
@@ -0,0 +1,54 @@
+//
+//  PhotoTakingHelper.swift
+//  Makestagram
+//
+//  Created by Benjamin Encz on 5/21/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+    import UIKit
+
+    typealias PhotoTakingHelperCallback = UIImage? -> Void
+
+    class PhotoTakingHelper : NSObject {
+      
+      /** View controller on which AlertViewController and UIImagePickerController are presented */
+      weak var viewController: UIViewController!
+      var callback: PhotoTakingHelperCallback
+      var imagePickerController: UIImagePickerController?
+      
+      init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
+        self.viewController = viewController
+        self.callback = callback
+        
+        super.init()
+        
+        showPhotoSourceSelection()
+      }
+      
+      func showPhotoSourceSelection() {
+        // Allow user to choose between photo library and camera
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        // Only show camera option if rear camera is available
+        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+          let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
+            // do nothing yet
+          }
+          
+          alertController.addAction(cameraAction)
+        }
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
+          // do nothing yet...
+        }
+        
+        alertController.addAction(photoLibraryAction)
+        
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+      }
+      
+    }
--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -6,29 +6,40 @@
 //  Copyright (c) 2015 Make School. All rights reserved.
 //
 
-    import UIKit
+import UIKit
 
-    class TimelineViewController: UIViewController {
+class TimelineViewController: UIViewController {
 
-      override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.tabBarController?.delegate = self
+  var photoTakingHelper: PhotoTakingHelper?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.tabBarController?.delegate = self
+  }
+  
+  // MARK: View callbacks
+  
+  func takePhoto() {
+    // instantiate photo taking class, provide callback for when photo is selected
+    photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
+      // don't do anything, yet...
+    })
+  }
+
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+  
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+      if (viewController is PhotoViewController) {
+        takePhoto()
+        return false
+      } else {
+        return true
       }
-
     }
-
-    // MARK: Tab Bar Delegate
-
-    extension TimelineViewController: UITabBarControllerDelegate {
-      
-      func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
-        if (viewController is PhotoViewController) {
-          println("Take Photo")
-          return false
-        } else {
-          return true
-        }
-      }
-      
-    }
+  
+}
