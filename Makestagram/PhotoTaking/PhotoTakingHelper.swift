b3638b5055df7c52c566804bd519bfd01a06fe05
Ben-G <Benjamin.Encz@gmail.com>
2015-05-22 12:34:38 -0700

    [STEP] Presenting the UIImagePickerController

--- Makestagram/PhotoTaking/PhotoTakingHelper.swift
+++ Makestagram/PhotoTaking/PhotoTakingHelper.swift
@@ -6,49 +6,55 @@
 //  Copyright (c) 2015 Make School. All rights reserved.
 //
 
-    import UIKit
+import UIKit
 
-    typealias PhotoTakingHelperCallback = UIImage? -> Void
+typealias PhotoTakingHelperCallback = UIImage? -> Void
 
-    class PhotoTakingHelper : NSObject {
-      
-      /** View controller on which AlertViewController and UIImagePickerController are presented */
-      weak var viewController: UIViewController!
-      var callback: PhotoTakingHelperCallback
-      var imagePickerController: UIImagePickerController?
-      
-      init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
-        self.viewController = viewController
-        self.callback = callback
-        
-        super.init()
-        
-        showPhotoSourceSelection()
+class PhotoTakingHelper : NSObject {
+  
+  /** View controller on which AlertViewController and UIImagePickerController are presented */
+  weak var viewController: UIViewController!
+  var callback: PhotoTakingHelperCallback
+  var imagePickerController: UIImagePickerController?
+  
+  init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
+    self.viewController = viewController
+    self.callback = callback
+    
+    super.init()
+    
+    showPhotoSourceSelection()
+  }
+  
+  func showPhotoSourceSelection() {
+    // Allow user to choose between photo library and camera
+    let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .ActionSheet)
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+    alertController.addAction(cancelAction)
+    
+    // Only show camera option if rear camera is available
+    if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+      let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
+        self.showImagePickerController(.Camera)
       }
       
-      func showPhotoSourceSelection() {
-        // Allow user to choose between photo library and camera
-        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .ActionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        alertController.addAction(cancelAction)
-        
-        // Only show camera option if rear camera is available
-        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
-          let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
-            // do nothing yet
-          }
-          
-          alertController.addAction(cameraAction)
-        }
-        
-        let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
-          // do nothing yet...
-        }
-        
-        alertController.addAction(photoLibraryAction)
-        
-        viewController.presentViewController(alertController, animated: true, completion: nil)
-      }
-      
+      alertController.addAction(cameraAction)
     }
+    
+    let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
+      self.showImagePickerController(.PhotoLibrary)
+    }
+    
+    alertController.addAction(photoLibraryAction)
+    
+    viewController.presentViewController(alertController, animated: true, completion: nil)
+  }
+  
+  func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
+    imagePickerController = UIImagePickerController()
+    imagePickerController!.sourceType = sourceType
+    self.viewController.presentViewController(imagePickerController!, animated: true, completion: nil)
+  }
+  
+}
