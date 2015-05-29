15dd6701b8c98faa2ffdf60ca8879cb8dbcc3e99
Ben-G <Benjamin.Encz@gmail.com>
2015-05-29 11:51:44 -0700

    [STEP] Add all required code connections

--- Makestagram/Views/Timeline/PostTableViewCell.swift
+++ Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -12,6 +12,10 @@ import Bond
 class PostTableViewCell: UITableViewCell {
 
   @IBOutlet weak var postImageView: UIImageView!
+  @IBOutlet weak var likesIconImageView: UIImageView!
+  @IBOutlet weak var likesLabel: UILabel!
+  @IBOutlet weak var likeButton: UIButton!
+  @IBOutlet weak var moreButton: UIButton!
   
   var post:Post? {
     didSet {
@@ -21,4 +25,13 @@ class PostTableViewCell: UITableViewCell {
       }
     }
   }
+  
+  @IBAction func moreButtonTapped(sender: AnyObject) {
+    
+  }
+  
+  @IBAction func likeButtonTapped(sender: AnyObject) {
+    
+  }
+  
 }
