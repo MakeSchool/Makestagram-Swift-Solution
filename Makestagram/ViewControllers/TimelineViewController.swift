610115de21367cf4fb77efbb197ad0eaf5e89fc0
Ben-G <Benjamin.Encz@gmail.com>
2015-06-05 14:25:28 -0700

    [STEP] Section Headers for Posts

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -80,14 +80,18 @@ extension TimelineViewController: UITabBarControllerDelegate {
 
 extension TimelineViewController: UITableViewDataSource {
   
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return self.timelineComponent.content.count
+  }
+  
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return timelineComponent.content.count
+    return 1
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
 
-    let post = timelineComponent.content[indexPath.row]
+    let post = timelineComponent.content[indexPath.section]
     post.downloadImage()
     post.fetchLikes()
     cell.post = post
@@ -103,7 +107,20 @@ extension TimelineViewController: UITableViewDelegate {
   
   func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
     
-    timelineComponent.targetWillDisplayEntry(indexPath.row)
+    timelineComponent.targetWillDisplayEntry(indexPath.section)
+  }
+  
+  func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let headerCell = tableView.dequeueReusableCellWithIdentifier("PostHeader") as! PostSectionHeaderView
+    
+    let post = self.timelineComponent.content[section]
+    headerCell.post = post
+    
+    return headerCell
+  }
+  
+  func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 40
   }
   
 }
---
+++ Makestagram/Views/Timeline/PostSectionHeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  PostSectionHeaderView.swift
+//  Makestagram
+//
+//  Created by Benjamin Encz on 6/5/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostSectionHeaderView: UITableViewCell {
+
+  @IBOutlet weak var postTimeLabel: UILabel!
+  @IBOutlet weak var usernameLabel: UILabel!
+  
+  var post: Post? {
+    didSet {
+      if let post = post {
+        usernameLabel.text = post.user?.username
+      }
+    }
+  }
+}
